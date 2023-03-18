55f1da78fb5bd2893c7a60bf353e0aad0d1a1b02
Jonathan Steve <jhuarca.peruapps@gmail.com>
2023-03-17 19:07:06 -0500

    add: initial project upload

---
+++ src/main/java/com/steve/market/SteveMarketApplication.java
@@ -0,0 +1,13 @@
+package com.steve.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteveMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SteveMarketApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/steve/market/persistence/crud/ProductoCrudRepository.java
@@ -0,0 +1,17 @@
+package com.steve.market.persistence.crud;
+
+import com.steve.market.persistence.entity.Producto;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ProductoCrudRepository extends CrudRepository<Producto, Integer> {
+    //Consulta en forma nativa
+    @Query(value = "SELECT * FROM productos WHERE id_categoria = ?", nativeQuery = true)
+    List<Producto> findByIdCategoria(int idCategoria);
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
+}
